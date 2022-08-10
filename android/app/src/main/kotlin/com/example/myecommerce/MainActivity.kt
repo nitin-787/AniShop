a10e0ace6131e3c720335e8f63cab75b64072bf7
Nitin Sharma <76255199+nitin-787@users.noreply.github.com>
2022-08-10 17:45:11 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/myecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color textColor = const Color(0xFFccc7c5);
+  static Color mainColor = const Color(0xFF89dad0);
+  // static Color mainColor = const Color(0xFFccc7c5);
+  static Color iconColor1 = const Color(0xFFccc7c5);
+  static Color iconColor2 = const Color(0xFFccc7c5);
+  static Color paraColor = const Color(0xFFccc7c5);
+  static Color buttonBackgroundColor = const Color(0xFFccc7c5);
+  static Color signColor = const Color(0xFFccc7c5);
+  static Color titleColor = const Color(0xFFccc7c5);
+  static Color mainBlackColor = const Color(0xFF332d2b);
+  // static Color yellowColor = const Color(0xFFccc7c5);
+  static Color yellowColor = const Color(0xFFccc7c5);
+}
