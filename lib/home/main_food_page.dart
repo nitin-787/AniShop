ea6e38bc087696d7ee92f89304f0e8aed110e975
Nitin Sharma <76255199+nitin-787@users.noreply.github.com>
2022-08-11 20:15:12 +0530

    sliding page view added

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:myecommerce/home/food_page_body.dart';
+import 'package:myecommerce/utils/colors.dart';
+import 'package:myecommerce/widgets/big_text.dart';
+import 'package:myecommerce/widgets/small_text.dart';
+
+class MyFoodPage extends StatefulWidget {
+  const MyFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyFoodPage> createState() => _MyFoodPageState();
+}
+
+class _MyFoodPageState extends State<MyFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "India",
+                        color: AppColors.mainColor,
+                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Solan",
+                            color: Colors.black54,
+                          ),
+                          const Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const FoodPageBody()
+        ],
+      ),
+    );
+  }
+}
